75d4309d1b5afe5137d1230f12facf06682eebe3
LMAR2025 <LMAR2025PUBLIC@gmail.com>
2025-05-22 20:39:08 -0400

    Update utils.py

--- code/utils.py
+++ code/utils.py
@@ -227,7 +227,7 @@ def set_seed(seed=42):
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)  # 如果你在用GPU
+    torch.cuda.manual_seed_all(seed) 
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
 
